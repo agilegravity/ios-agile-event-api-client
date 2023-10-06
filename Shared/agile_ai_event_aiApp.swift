6d3ba7a099ce0793e657097b2d55db1d79ab3215
Benedikt Weiner <weiner@agilegravity.com>
2023-10-06 20:43:49 +0200

    Initial Commit

---
+++ Shared/agile_ai_event_aiApp.swift
@@ -0,0 +1,17 @@
+//
+//  agile_ai_event_aiApp.swift
+//  Shared
+//
+//  Created by Benedikt Weiner on 06.10.23.
+//
+
+import SwiftUI
+
+@main
+struct agile_ai_event_aiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
